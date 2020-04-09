340cace2c88d102a566d22058de672e6abcebf7e
Charlotte Ellwood <char502@hotmail.com>
2020-04-09 11:54:07 +0100

    first commi

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Layout, Button } from "antd";
+import "./App.css";
+
+const { Header } = Layout;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Layout>
+        <Header className="header">
+          <h1 className="title">This is the title</h1>
+        </Header>
+      </Layout>
+      <Button>This is a button</Button>
     </div>
   );
 }
